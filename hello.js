785a9c23ed94fa91d9e35c5043313686999f7bf7
Taylor <taylorscott@hotmail.es>
2023-05-17 22:41:06 -0500

    solucion a los ejercicios de js

---
+++ hello.js
@@ -0,0 +1,6 @@
+function hola(name) {
+    return "Hola " + name +"!";
+}
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
---
+++ sum.js
@@ -0,0 +1,10 @@
+function sum(x){
+    var suma = 0
+    for (var i=1; i<=x; i++){
+        suma += i
+         }
+    return suma
+    }
+    console.log(sum(4));
+    console.log(sum(10));
+    console.log(sum(15));
